df70ee20365ca1f1c98c1eafa2c9c44828a69e02
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-25 13:16:02 -0600

    category-routes.js: completed find one category by its 'id' value

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,15 +17,23 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   try {
-    const categoryData = await Category.findByPk(req.params.id,{include: [{ model: Product }],});
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No location found with this id!" });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
   }
-  if(!categoryData){
-    res.status(404).json({message: 'No location found with this id!'});
-    return;
-  }
+
   // be sure to include its associated Products
 });
 
