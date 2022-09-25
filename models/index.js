b83af6aa73677c8ec42c0d92375dadecd41f2ea9
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-25 11:13:39 -0600

    index.js: added category.hasmany products column

--- models/index.js
+++ models/index.js
@@ -10,6 +10,10 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
 
