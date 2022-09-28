ebb2b5724687ed437b61fbe850b853a86564b8c1
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-28 13:32:02 -0600

    added find single tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,7 +20,20 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+      include: [
+        {
+          model: Product,
+          through: ProductTag,
+        },
+      ],
+    },
+  })
+    // be sure to include its associated Product data
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 router.post("/", (req, res) => {
